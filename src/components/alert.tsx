bc4af3263f7bd94dbf00a8719f0b12bcc4c8adc9
ImSoLostEXE <j.forline1@gmail.com>
2024-05-12 18:50:56 -0500

    first commit

---
+++ src/components/alert.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+export const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-secondary alert-dismissible ">
+      <h4 className="alert-heading">Nothing happened.</h4>
+      <hr />
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
