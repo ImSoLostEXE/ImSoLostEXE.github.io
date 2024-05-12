82eb63d8e5a990ceca27f414e760a33f5fe12dd1
ImSoLostEXE <j.forline1@gmail.com>
2024-05-12 05:31:51 -0500

    add: impulse workflow

---
+++ src/components/spinner.tsx
@@ -0,0 +1,10 @@
+
+
+export const Spinner = () => {
+    return (
+    <div className="d-flex align-items-center">
+        <strong role="status">Screaming into the void...</strong>
+        <div className="spinner-border ms-auto" aria-hidden="true"></div>
+    </div>
+  )
+}
